5402ea6c75699f4ee3d38fb784b3f414a722602c
StiIiyan <ssossess@abv.bg>
2026-08-10 16:27:59 +0300

    Fix compatibility without footprint

--- crossmod.lua
+++ crossmod.lua
@@ -1,4 +1,4 @@
-local old_footprint_func = SMODS.DrawStep.obj_table["mxms_footprint"].func
+local old_footprint_func = SMODS.DrawStep.obj_table["mxms_footprint"] and SMODS.DrawStep.obj_table["mxms_footprint"].func
 SMODS.DrawStep:take_ownership("mxms_footprint", {
     func = function(card, context)
         if Mace.is_using_skin(card) then
